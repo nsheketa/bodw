d8cc590a4dd2275d47d765458f6d02d479da42ac
NSheketa <natalya.sheketa@gmail.com>
2020-06-25 01:19:25 +0300

    Homepage section done

--- js/main.js
+++ js/main.js
@@ -102,6 +102,33 @@ $(document).ready(function () {
     pauseOnDotsHover: true,
   });
 
+  $('.partners__carousel').slick({
+    dots: false,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    adaptiveHeight: false,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        }
+      }
+    ]
+  });
+
   /* Trigger resize once */
   $(window).resize(function () {
     clearTimeout(resizeId);
